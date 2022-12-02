03fab7a865c496fe5d8aad48afa6326132e0bbab
edelweiss2 <yunhokim2000@gmail.com>
2022-12-02 11:09:57 +0900

    2022-12-02

---
+++ 05.Javascript/ch08.예외/01.기본처리.js
@@ -0,0 +1,8 @@
+//         // ReferenceError: error is not defined
+
+try {
+    error.error.error();
+} catch(e) {
+    console.log(e.name);        // ReferenceError
+    console.log(e.message);     // error is not defined
+}
