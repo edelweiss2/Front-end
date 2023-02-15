395a72664b3f8552828384de650d1afba7255b35
edelweiss2 <yunhokim2000@gmail.com>
2023-02-15 15:52:08 +0900

    123

--- 종합예제/goodM.js
+++ 종합예제/goodM.js
@@ -1,44 +1,9 @@
+var searchData = [];
+var searchDataObj = {
+    title : ads,
+    address : 
+};
 
 
-function searchPlaces() {
-    searchData.forEach(function(addr, index) {
-        geocoder.addressSearch(addr, function(result, status) {
-            if (status === daum.maps.services.Status.OK) {
-                var coords = new daum.maps.LatLng(result[0].y, result[0].x);
-    
-                var marker = new daum.maps.Marker({
-                    map: map,
-                    position: coords
-                });  
-                
-                map.setCenter(coords);
-            } 
-        });
-    });
-} 
 
-
-function placesSearchCB(data, status, pagination) {
-    if (status === kakao.maps.services.Status.OK) {
-
-        // 정상적으로 검색이 완료됐으면
-        // 검색 목록과 마커를 표출합니다
-        displayPlaces(data);
-
-        // 페이지 번호를 표출합니다
-        displayPagination(pagination);
-
-    } else if (status === kakao.maps.services.Status.ZERO_RESULT) {
-
-        alert('검색 결과가 존재하지 않습니다.');
-        return;
-
-    } else if (status === kakao.maps.services.Status.ERROR) {
-
-        alert('검색 결과 중 오류가 발생했습니다.');
-        return;
-
-    }
-}
-
-
+console.log(searchData);
